f1601f48668614417492a59c64f52bcdf23635cc
Glendon Small <gsmall@sonovate.com>
2019-08-07 22:49:44 +0100

    App.json for pipeline test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Inc
+                    Just Laravel
                 </div>
 
                 <div class="links">
